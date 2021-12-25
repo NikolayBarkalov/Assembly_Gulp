747fb1ac79b6322194b26e2f4bb0c5e5eab6c7e5
Nikolay Barkalov <nikolya.barkalov.web@gmail.com>
2021-12-25 20:18:10 +0300

    Mod. gulpfile series

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,46 @@
-function defaultTask(cb) {
-	// Разместите здесь код дефолтного таска
+const { series, parallel } = require('gulp');
+ 
+function clean(cb) {
+	// тело функции
 	cb();
 }
  
-exports.default = defaultTask
+function cssTranspile(cb) {
+	// тело функции
+	cb();
+}
+ 
+function cssMinify(cb) {
+	// тело функции
+	cb();
+}
+ 
+function jsTranspile(cb) {
+	// тело функции
+	cb();
+}
+ 
+function jsBundle(cb) {
+	// тело функции
+	cb();
+}
+ 
+function jsMinify(cb) {
+	// тело функции
+	cb();
+}
+ 
+function publish(cb) {
+	// тело функции
+	cb();
+}
+ 
+exports.build = series(
+	clean,
+	parallel(
+		cssTranspile,
+		series(jsTranspile, jsBundle)
+	),
+	parallel(cssMinify, jsMinify),
+	publish
+);
