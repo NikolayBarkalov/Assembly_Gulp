8ae57a63eb3b6dec90d0943dbe33ada02908576e
Nikolay Barkalov <nikolya.barkalov.web@gmail.com>
2021-12-25 22:04:33 +0300

    full task manager

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,48 @@
-const { series, parallel } = require('gulp');
- 
-function clean(cb) {
-	// тело функции
-	cb();
-}
- 
-function cssTranspile(cb) {
-	// тело функции
-	cb();
-}
- 
-function cssMinify(cb) {
-	// тело функции
-	cb();
-}
- 
-function jsTranspile(cb) {
-	// тело функции
-	cb();
-}
- 
-function jsBundle(cb) {
-	// тело функции
-	cb();
-}
- 
-function jsMinify(cb) {
-	// тело функции
-	cb();
-}
- 
-function publish(cb) {
-	// тело функции
-	cb();
-}
- 
-exports.build = series(
-	clean,
-	parallel(
-		cssTranspile,
-		series(jsTranspile, jsBundle)
-	),
-	parallel(cssMinify, jsMinify),
-	publish
-);
+var gulp = require("gulp"), // Подключаем Gulp
+  sass = require("gulp-sass")(require("sass")), //Подключаем Sass пакет,
+  browserSync = require("browser-sync"); // Подключаем Browser Sync
+
+var gulp = require("gulp"), // Подключаем Gulp
+  sass = require("gulp-sass")(require("sass")), //Подключаем Sass пакет,
+  browserSync = require("browser-sync"), // Подключаем Browser Sync
+  concat = require("gulp-concat"), // Подключаем gulp-concat (для конкатенации файлов)
+  uglify = require("gulp-uglifyjs"); // Подключаем gulp-uglifyjs (для сжатия JS)
+
+gulp.task("sass", function () {
+  // Создаем таск Sass
+  return gulp
+    .src("app/sass/**/*.sass") // Берем источник
+    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(gulp.dest("app/css")) // Выгружаем результата в папку app/css
+    .pipe(browserSync.reload({ stream: true })); // Обновляем CSS на странице при изменении
+});
+
+gulp.task("browser-sync", function () {
+  // Создаем таск browser-sync
+  browserSync({
+    // Выполняем browserSync
+    server: {
+      // Определяем параметры сервера
+      baseDir: "app", // Директория для сервера - app
+    },
+    notify: false, // Отключаем уведомления
+  });
+});
+
+//Cоздаем задачу для сборки и сжатия всех библиотек (перед watch):
+gulp.task("scripts", function () {
+  return gulp
+    .src([
+      // Берем все необходимые библиотеки
+      "app/libs/jquery/dist/jquery.min.js", // Берем jQuery
+      "app/libs/magnific-popup/dist/jquery.magnific-popup.min.js", // Берем Magnific Popup
+    ])
+    .pipe(concat("libs.min.js")) // Собираем их в кучу в новом файле libs.min.js
+    .pipe(uglify()) // Сжимаем JS файл
+    .pipe(gulp.dest("app/js")); // Выгружаем в папку app/js
+});
+
+gulp.task("watch", function () {
+  gulp.watch("app/sass/**/*.sass", gulp.parallel("sass")); // Наблюдение за sass файлами
+});
+gulp.task("default", gulp.parallel("sass", "browser-sync", "watch"));
