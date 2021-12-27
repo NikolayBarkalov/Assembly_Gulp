ae744cb1798ea4669ec3f510bd69b2519dd5ebda
Nikolay Barkalov <nikolya.barkalov.web@gmail.com>
2021-12-27 22:45:03 +0300

    add & testing Gulp-htmlmin COOL!!!

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const {src, dest} = require("gulp");
 
 // Плагины
-const fileInclude = require("gulp-file-include")
+const fileInclude = require("gulp-file-include");
+const htmlmin = require("gulp-htmlmin")
 
 // Обработка HTML
 const html = () => {
   return src("./src/html/*.html")
   .pipe(fileInclude())
+  .pipe(htmlmin({
+    collapseWhitespace: true //убрать символы пробела
+  }))
   .pipe(dest("./public"));
 };
 
