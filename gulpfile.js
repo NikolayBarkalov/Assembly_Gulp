6861ec36d3396f1020bf390d62fa1ca4c69fb6b7
Nikolay Barkalov <nikolya.barkalov.web@gmail.com>
2021-12-28 19:46:25 +0300

    add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,17 @@ const {src, dest} = require("gulp");
 // Плагины
 const fileInclude = require("gulp-file-include");
 const htmlmin = require("gulp-htmlmin")
+const size = require("gulp-size")
 
 // Обработка HTML
 const html = () => {
   return src("./src/html/*.html")
   .pipe(fileInclude())
+  .pipe(size({ title: "До сжатия" }))
   .pipe(htmlmin({
-    collapseWhitespace: true //убрать символы пробела
+    collapseWhitespace: true //убрать символы пробела минимилизация html
   }))
+  .pipe(size({ title: "После сжатия" }))
   .pipe(dest("./public"));
 };
 
